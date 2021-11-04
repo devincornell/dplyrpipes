23c256fc22f14eb392697185f3676cec4d1e242c
devincornell <devincornell@example.com>
2021-11-04 12:16:11 -0400

    working?

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 
-from typing import Callable, Any, Union
+from typing import Callable, Any, Union, List
 import functools
 
 import dataclasses
@@ -9,25 +9,60 @@ class CompFunc:
         self.func = func
         self.args = args
         self.kwargs = kwargs
-        self.other_funcs = list()
-    
-    def __call__(self, input: Any) -> Any:
-        '''Actually call function.'''
-        x = self.func(input, *self.args, **self.kwargs)
-        for other_func in self.other_funcs:
-            if not isinstance(other_func, out):
-                x = other_func(x)
-        return x
+        self.other_compfuncs = list()
+
+    @property
+    def is_null(self):
+        return self.func is None
+
+    def __call__(self, data: Any) -> Any:
+        '''Call self.func on the data.
+        '''
+        return self.func(data, *self.args, **self.kwargs)
+
+    def execute_all(self, compdata, inplace: bool = False):
+        '''Execute all functions on the input data sequentially.
+        '''
+        all_compfuncs = [self] + self.other_compfuncs
+
+        x = compdata.data
+        for compfunc in all_compfuncs:
+            if not compfunc.is_null:
+                x = compfunc(x)
+            else:
+                return x
+
+        if inplace:
+            compdata.data = x
+            return compdata
+        else:
+            return compdata.__class__(x)
 
     def __rshift__(self, other_compfunc):
         '''Aggregate component functions.
         '''
-        self.other_funcs.append(other_compfunc)
+        self.other_compfuncs.append(other_compfunc)
         return self
     
     def __repr__(self):
-        return f'{self.__class__.__name__}({self.func}, {", ".join(map(str,self.args))})'
+        argstr = ", ".join(map(str,self.args))
+        kwargstr = ", ".join(f'{k}={v}' for k,v in self.kwargs.items())
+        return f'{self.__class__.__name__}({self.func}, {argstr}, {kwargstr})'
 
+class out(CompFunc):
+    '''Used at the end of a pipeline to output data.'''
+    def __init__(self):
+        super().__init__(func=None)
+    def __rshift__(self, other_compfunc):
+        raise ValueError('Cannot pipe data after the out().')
+
+@dataclasses.dataclass
+class CompData:
+    '''Store data and overload operator to apply component functions.'''
+    data: Any
+
+    def __rshift__(self, compfunc):
+        return compfunc.execute_all(self)
 
 def comp_decorator(func):
     def wrapper_comp_decorator(*args, **kwargs):
@@ -35,51 +70,48 @@ def comp_decorator(func):
     return wrapper_comp_decorator
 
 
-@dataclasses.dataclass
-class CompData:
-    data: Any
-    def __rshift__(self, compfunc: CompFunc):
-        if isinstance(compfunc, out):
-            return self.data
-        return self.__class__(compfunc(self.data))
-
-class out:
-    def __call__(self, *args, **kwargs):
-        return None
-
+###################### dplyr equivalent overloads ####################
 
 @comp_decorator
-def test_func(x, y: int = 1):
-    return x * y
+def filter(df, *args, **kwargs):
+    return df.query(*args, **kwargs)
 
+@comp_decorator
+def select(df, *colnames):
+    return df[colnames]
+
+@comp_decorator
+def arrange(df, *colnames, **kwargs):
+    return df.sort_values(colnames, **kwargs)
+
+@comp_decorator
+def rename(df, *args, **kwargs):
+    return df.rename(*args, **kwargs)
+
+@comp_decorator
+def mutate(df, *args, **kwargs):
+    return df.assign(*args, **kwargs)
+
+@comp_decorator
+def summary(df, *args, **kwargs):
+    return df.describe(*args, **kwargs)
+
+@comp_decorator
+def passthrough(x):
+    return x
 
 if __name__ == '__main__':
-    
-    out = CompData(5) >> (
-        test_func(2) >> 
+    import pandas as pd
+    df = pd.DataFrame([
+        {'name': 'Karl', 'age': 7}, 
+        {'name': 'Sandra', 'age': 10}, 
+        {'name': 'Chris', 'age': 12}
+    ])
+
+    data = CompData(df) >> (
+        #mutate(birthyear = 2021-df['age']) >>
+        passthrough() >>
         out()
     )
-    print(out)
+    print(data)
 
-    #print(my_comp(test_func(2)))
-    print(type(test_func))
-    print(type(test_func()))
-    
-    comp = test_func(y=5)
-    print(comp(2))
-    #print(CompData(2) >> test_func)
-
-
-    # CompData: used to pass data
-    # CompFunc: operates on CompData
-
-
-    #say_whee(lambda x: x == 5)
-    #say_whee()
-    #say_whee()
-
-    #pipestart(x) &\
-    #    myfilt(x = 1)
-
-    #startpipe + x >>\
-    #myfilter(lambda x: x * 2)
